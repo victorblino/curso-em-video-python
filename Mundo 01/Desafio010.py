2cf41ae84f0484e9efb09567283a7ec042ec3492
admvicli <admvicli@gmail.com>
2021-02-15 19:23:07 -0300

    Desafio 10 - Adicionado valor do dólar no comentário (nada de relevante)

--- Mundo 01/Desafio010.py
+++ Mundo 01/Desafio010.py
@@ -1,10 +1,10 @@
 #Desafio0010
 
-print('Converta dólares você tem na carteira: ')
+print('Converta dólares você tem na carteira (cotação 2017, sdds...): ')
 
 
 valor = float(input('Quanto você tem? '))
-dolar = float('3.27')
+dolar = float('3.27') # 5.37 / dia: 15/02/2021 :(
 
 print('Você tem {} dólares na carteira'.format(valor*dolar))
 
