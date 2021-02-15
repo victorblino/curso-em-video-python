5552d02464757e5c24417d3374ae2434f8c7c684
admvicli <admvicli@gmail.com>
2021-02-15 14:49:27 -0300

    Desafio 001 e 002 feitos.

--- Mundo 01/001.py
+++ Mundo 01/001.py
@@ -1,1 +1,3 @@
+#Desafio001
+
 print('Olá mundo!') # Ou Hello World para os íntimos rsrs.
---
+++ Mundo 01/002.py
@@ -0,0 +1,4 @@
+# Desafio 002
+
+nome = input('Olá, qual o seu nome? ')
+print('Seja bem vindo', nome + '!')
