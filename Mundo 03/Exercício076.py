51a60f7fb1be2649baf2d7065b370d4272a7d238
Victor B. Lino <40004009+ADMVicli@users.noreply.github.com>
2021-06-29 17:27:33 -0300

    Exercício 076 atualizado

--- Mundo 03/Exercício076.py
+++ Mundo 03/Exercício076.py
@@ -7,8 +7,6 @@ valores = (
     int(input('Insira o último: '))
 )
 
-cont = 0
-
 print(f'O número nove apareceu {valores.count(9)} vezes.')
 if 3 in valores:
     print(f'O valor 3 foi digitado na {valores.index(3)+1} posição.')
@@ -19,4 +17,3 @@ print('Os valores pares digitados, foram: ', end='')
 for n in valores:
     if n % 2 == 0:
         print(n, end=' ')
-
